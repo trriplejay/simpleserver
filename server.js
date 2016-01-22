1599d9f58d01fc2cb90f4fad835c9f4c6dd08fcf
John Stockbauer <john@shippable.com>
2016-01-21 16:43:22 -0800

    listen to a second port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 var express = require('express');
 var app = express();
+var app2 = express();
 
 console.log('starting');
 
@@ -10,6 +11,16 @@ app.get('/', function (req, res) {
   res.send(out);
 });
 
+app2.get('/', function (req, res) {
+  var message = 'admin use only';
+  res.send(message);
+});
+
 var port = process.env.PORT || 8888;
 console.log('Listening on port: ' + port);
 app.listen(port);
+
+
+var adminPort = process.env.ADMIN_PORT || 12345;
+console.log('Listening on port: ' + adminPort);
+app2.listen(adminPort);
