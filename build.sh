68baa0a596f4e0e27edf6db2a8f2c033ed74958b
John Stockbauer <john@shippable.com>
2016-02-08 22:12:58 -0800

    new yml

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+if [ "$IS_PULL_REQUEST" != true ]; then
+  sudo docker build -t trriplejay/simpleserver:$BRANCH.$SHIPPABLE_BUILD_NUMBER .
+  sudo docker push trriplejay/simpleserver:$BRANCH.$SHIPPABLE_BUILD_NUMBER
+else
+  echo "skipping because it's a PR"
+fi
