0ba2d2acf7758509dd3c35529a4ffe47320d6103
John Stockbauer <john@shippable.com>
2016-02-15 11:02:25 -0800

    put build stuff directly in yml

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-if [ "$IS_PULL_REQUEST" != true ]; then
-  sudo docker build -t trriplejay/simpleserver:$BRANCH.$SHIPPABLE_BUILD_NUMBER \
-    --build-arg mytag=$BRANCH.$SHIPPABLE_BUILD_NUMBER .
-  sudo docker push trriplejay/simpleserver:$BRANCH.$SHIPPABLE_BUILD_NUMBER
-else
-  echo "skipping because it's a PR"
-fi
