a0c885eb552e4eade5c28a2a61ce282d264ec124
John Stockbauer <john@shippable.com>
2016-03-17 15:40:47 -0700

    normal script

--- testExit.sh
+++ testExit.sh
@@ -1,1 +1,13 @@
-#!/bin/bash set -e if [ "$TEST" == "YES" ]; then echo "do I see this?" echo "how" echo "many" echo "lines" echo "does" echo "it" echo "take" exit 0 else echo "no exit" fi
+#!/bin/bash set -e
+if [ "$TEST" == "YES" ]; then
+  echo "do I see this?"
+  echo "how"
+  echo "many"
+  echo "lines"
+  echo "does"
+  echo "it"
+  echo "take"
+  exit 0
+else
+  echo "no exit"
+fi
