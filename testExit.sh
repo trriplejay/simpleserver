08bf8574956918fae5e9285fdc2a924603494033
John Stockbauer <john@shippable.com>
2016-03-17 15:37:59 -0700

    test single line script

--- testExit.sh
+++ testExit.sh
@@ -1,13 +1,1 @@
-#!/bin/bash set -e
-if [ "$TEST" == "YES" ]; then
-  echo "do I see this?"
-  echo "how"
-  echo "many"
-  echo "lines"
-  echo "does"
-  echo "it"
-  echo "take"
-  exit 0
-else
-  echo "no exit"
-fi
+#!/bin/bash set -e if [ "$TEST" == "YES" ]; then echo "do I see this?" echo "how" echo "many" echo "lines" echo "does" echo "it" echo "take" exit 0 else echo "no exit" fi
