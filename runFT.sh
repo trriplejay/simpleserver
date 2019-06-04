36e27ceaed5721558d073f2f4890ecc226db8865
trriplejay <johns@jfrog.com>
2019-06-04 15:28:34 -0700

    make some changes

--- runFT.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -u
-
-declare -a blah=(${MYENV})
-echo $blah
--- server.js
+++ server.js
@@ -1,36 +1,30 @@
 'use strict';
 var express = require('express');
 var app = express();
-var app2 = express();
 
 console.log('starting');
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   console.log('got a request');
-  var out = process.env.FOO;
-  var tag = process.env.BUILD_NUMBER || 'no tag found';
-  var response = '<html><h2>FOO=' + out + '</h2><h3>' +
-    'tag:' + tag + '</h3></html>';
+  var tag = process.env.STEP_ID || 'no tag found';
+  var sha = process.env.COMMIT || 'no commit found';
+  var response =
+    '<html></h2><h3>' +
+    'tag: ' +
+    tag +
+    '</h3><h3>commit: ' +
+    sha +
+    '</h3></html>';
 
   res.send(response);
 });
 
-app.get('/tag', function (req, res) {
+app.get('/tag', function(req, res) {
   console.log('got a tag request');
   var tag = process.env.BUILD_NUMBER || 'no build in environment';
   res.send(tag);
 });
 
-app2.get('/', function (req, res) {
-  var message = 'admin use only';
-  res.send(message);
-});
-
 var port = process.env.PORT || 8888;
 console.log('Listening on port: ' + port);
 app.listen(port);
-
-
-var adminPort = process.env.ADMIN_PORT || 12345;
-console.log('Listening on port: ' + adminPort);
-app2.listen(adminPort);
