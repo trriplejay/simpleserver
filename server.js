2a7c79740955f1024efc49ac839d774a6a1c4c6e
trriplejay <johns@jfrog.com>
2019-10-02 08:29:26 -0700

    change message

--- server.js
+++ server.js
@@ -6,14 +6,8 @@ console.log('starting');
 
 app.get('/', function(req, res) {
   console.log('got a request');
-  var response ='<html><h2>Hello, world!</h2></html>'
+  var response ='<html><h2>Hello, frogs!</h2></html>'
   res.send(response);
-});
-
-app.get('/tag', function(req, res) {
-  console.log('got a tag request');
-  var tag = process.env.BUILD_NUMBER || 'no build in environment';
-  res.send(tag);
 });
 
 var port = process.env.PORT || 8888;
