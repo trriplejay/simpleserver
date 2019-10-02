1b4927e6fe9aa6f34dae5ca390cdd7c01f950f26
trriplejay <johns@jfrog.com>
2019-10-01 21:14:36 -0700

    update app

--- server.js
+++ server.js
@@ -6,16 +6,7 @@ console.log('starting');
 
 app.get('/', function(req, res) {
   console.log('got a request');
-  var tag = process.env.STEP_ID || 'no tag found';
-  var sha = process.env.COMMIT || 'no commit found';
-  var response =
-    '<html></h2><h3>' +
-    'tag: ' +
-    tag +
-    '</h3><h3>commit: ' +
-    sha +
-    '</h3></html>';
-
+  var response ='<html><h2>Hello, world!</h2></html>'
   res.send(response);
 });
 
