71fac516573b37930bfd56c77766646ce388cd53
John Stockbauer <johns@matrixd.io>
2023-10-31 13:31:06 -0700

    some old package adjustments

--- micro/svcB/server.js
+++ micro/svcB/server.js
@@ -1,15 +1,15 @@
-'use strict';
-var express = require('express');
+"use strict";
+var express = require("express");
 var app = express();
 // blah1
-console.log('starting');
+console.log("starting");
 
-app.get('/', function(req, res) {
-  console.log('got a request');
-  var response ='<html><h2>Hello, frogs!</h2><h4>from service B</h4</html>';
+app.get("/", function(req, res) {
+  console.log("got a request");
+  var response = "<html><h2>Hello world!</h2><h4>from service B</h4</html>";
   res.send(response);
 });
 
 var port = process.env.PORT || 8802;
-console.log('Listening on port: ' + port);
+console.log("Listening on port: " + port);
 app.listen(port);
